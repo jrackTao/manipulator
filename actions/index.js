6ba264a3c2dd96e5830ae331cad8361689e9e396
jrackson <727602601@qq.com>
2019-08-30 16:36:57 +0800

    ;,;l

--- actions/index.js
+++ actions/index.js
@@ -2,25 +2,33 @@ const robot = require('robotjs')
 
 const 按下键位 = []
 
-const 按键 = function (key) {
-  robot.keyToggle(key, 'down');
-  robot.keyToggle(key, 'up');
-}
-
-const 按下 = function (key) {
-  const t = setInterval(() => {
+xconst 按键 = function(key) {
     robot.keyToggle(key, 'down');
     robot.keyToggle(key, 'up');
-  }, 50);
-  按下键位[key] = t;
 }
 
-const 松开 = function (key) {
-  clearInterval(按下键位[key]);
+const 按下 = function(key) {
+    robot.keyToggle(key, 'down');
 }
+
+// const 按下 = function (key) {
+//   const t = setInterval(() => {
+//     robot.keyToggle(key, 'down');
+//     robot.keyToggle(key, 'up');
+//   }, 50);
+//   按下键位[key] = t;
+// }
+
+const 松开 = function(key) {
+    robot.keyToggle(key, 'up');
+}
+
+// const 松开 = function(key) {
+//     clearInterval(按下键位[key]);
+// }
 
 module.exports = {
-  按键,
-  按下,
-  松开,
+    按键,
+    按下,
+    松开,
 }
--- index.js
+++ index.js
@@ -2,42 +2,42 @@ const robot = require("robotjs")
 const constant = require('./constant')
 const action = require('./actions')
 const screen = require('./screen')
-const {sleep} = require('./utils')
+const { sleep } = require('./utils')
 
-const doing = async () => {
-  // action.按下(constant.移动左);
-  // await sleep(5);
-  // action.松开(constant.移动左);
-  const img = screen.捕捉屏幕({x: 0, y: 50, w: 600, h: 600});
-  const finds = screen.获取像素位置(img, ['0a0909', 'fffd37'], 20);
-  console.log(finds);
-  if(finds.length != 2) {
-    console.log('没找到');
-  } else {
+const doing = async() => {
+    action.按下(constant.移动右);
+    action.按下(constant.普通攻击);
 
-    if(finds[1].y === finds[0].y) {
-      console.log('干');
-    } else {
-      if(finds[1].y > finds[0].y) {
-        console.log('向下走');
-      } else {
-        console.log('向上走');
-      }
-    }
-    // if(finds[1].x > finds[0].x) {
-    //   console.log('向右走');
+    await sleep(5);
+    action.松开(constant.移动右);
+    // const img = screen.捕捉屏幕({x: 0, y: 50, w: 600, h: 600});
+    // const finds = screen.获取像素位置(img, ['0a0909', 'fffd37'], 20);
+    // console.log(finds);
+    // if(finds.length != 2) {
+    //   console.log('没找到');
     // } else {
-    //   console.log('向左走')
+
+    //   if(finds[1].y === finds[0].y) {
+    //     console.log('干');
+    //   } else {
+    //     if(finds[1].y > finds[0].y) {
+    //       console.log('向下走');
+    //     } else {
+    //       console.log('向上走');
+    //     }
+    //   }
+    //   // if(finds[1].x > finds[0].x) {
+    //   //   console.log('向右走');
+    //   // } else {
+    //   //   console.log('向左走')
+    //   // }
     // }
-  }
 
-  await sleep(.1);
+    // await sleep(.1);
 
-  await doing();
+    // await doing();
 }
 
 setTimeout(() => {
-  doing();
+    doing();
 }, 1000);
-
-
